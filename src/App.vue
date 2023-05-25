16233cd60eb097cbeeaf53fb6e7e541105fe21d0
TommasoPezzola <pezzola123@gmail.com>
2023-05-25 14:50:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+</script>
+
+<template>
+  <AppHeader />
+
+  <AppMain />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+    <AppHeader />
+
+    <AppMain />
+</template>
+
+<style lang="scss" scoped></style>
